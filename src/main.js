1c6276827e08c4a84a1cd0273d3c2b77411e7282
Giang <giang@Toans-Mac-mini.local>
2020-11-17 09:48:39 +0700

    starting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import './plugins/fontawesome'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import AppButton from './components/AppButton'
+
+Vue.component('AppButton', AppButton)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Board.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="board">
+    <div class="flex flex-row items-start">
+      <div class="column">
+        <div class="flex items-center mb-2 font-bold"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: mapState(["board"]),
+};
+</script>
+
+<style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+.column {
+  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
+.board {
+  @apply p-4 bg-teal-dark h-full overflow-auto;
+}
+
+.task-bg {
+  @apply pin absolute;
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
